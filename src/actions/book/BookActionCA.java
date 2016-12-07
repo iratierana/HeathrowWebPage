231c384318e293865c7798b768f9c67d25c3feca
Mikel Arizmendiarrieta <mikel.arizmendiarrie@alumni.mondragon.edu>
2016-12-07 08:30:58 +0100

    add BookActionCA

---
+++ src/actions/book/BookActionCA.java
@@ -0,0 +1,19 @@
+package actions.book;
+
+public class BookActionCA {
+	
+	private String account;
+	
+	public String execute(){
+		return "bookCA";
+	}
+
+	public String getAccount() {
+		return account;
+	}
+
+	public void setAccount(String account) {
+		this.account = account;
+	}
+
+}
