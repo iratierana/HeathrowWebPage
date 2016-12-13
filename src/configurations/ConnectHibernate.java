87a61320d5122b4e4802a51f434dab778fac54a7
xabier <xabier.jauregi@alumni.mondragon.edu>
2016-12-13 15:53:41 +0100

    Add javadoc to ConnectHibernate.java

--- src/configurations/ConnectHibernate.java
+++ src/configurations/ConnectHibernate.java
@@ -4,12 +4,27 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+
+/**
+ * 
+ * @author Xabier Jauregi
+ * @author Irati Eraña
+ * @author Mikel Arizmendiarrieta 
+ * @version 1.0
+ * @since   2016-12-13
+ * 
+ * Class where are all the needed functions related with the connection with the database.
+ *  
+ */
 public class ConnectHibernate {
 	
 	private static SessionFactory sessionFactory;
 	private static Session session;
 	private final static String CONFIG_FILE = "configurations/hibernate.cfg.xml";
 	
+	/**
+	 * This function create or opens the remote connection(session) with the database, loading the configuration file
+	 */
 	public static void before(){
 		
 		sessionFactory  = new Configuration().
@@ -18,6 +33,9 @@ public class ConnectHibernate {
 		
 	}
 	
+	/**
+	 * This function close or kills the connection(session) with the database
+	 */
 	public static void after(){
 		
 		session.close();
