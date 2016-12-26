e77e7a21ed49dd70cbcb2d3ac8af563e85a561c3
xabier <xabier.jauregi@alumni.mondragon.edu>
2016-12-26 14:23:18 +0100

    Create a function to load the photo list

--- src/domain/dao/DAOAirplanePhoto.java
+++ src/domain/dao/DAOAirplanePhoto.java
@@ -2,101 +2,51 @@ package domain.dao;
 
 import java.util.List;
 
-import javax.persistence.TypedQuery;
+import javax.persistence.Query;
 
 import org.hibernate.Session;
 
 import configurations.ConnectHibernate;
 import domain.model.AirplanePhoto;
-
-public class DAOAirplanePhoto {
-
-	/**
-	 * 
-	 * @author Xabier Jauregi
-	 * @author Irati Eraña
-	 * @author Mikel Arizmendiarrieta 
-	 * @version 1.0
-	 * @since   2016-12-13
-	 * 
-	 * Class where are all the needed functions related with AirplanePhoto, in order to work with the database
-	 *  
-	 */
-private static Session session;
-	
 /**
  * 
- * This function insert a photo in the database
+ * @author Xabier Jauregi
+ * @author Irati Eraña
+ * @author Mikel Arizmendiarrieta 
+ * @version 1.0
+ * @since   2016-12-13
  * 
- * @param photo the photo to insert in the database
- * @return true if the insert is correct
- * @return false if and error occurs during the insert
+ * Class where are all the needed functions related with AirplanePhoto, in order to work with the database
+ *  
  */
-	public static boolean insertAirplanePhoto(AirplanePhoto photo){
-		try {
-			
-			ConnectHibernate.before();
-			session = ConnectHibernate.getSession();
-			session.getTransaction().begin();
-			session.save(photo); //erlazinuak itxen dianian save kendu eta persist ipinibiada eta eralazinuan cascade cascade type.persist
-			session.getTransaction().commit();
-			ConnectHibernate.after();
-			return true;
-			
-		} catch (Exception e) {
-			session.getTransaction().rollback();
-			ConnectHibernate.after();
-			return false;		
-		}
-	}
+public class DAOAirplanePhoto {
 
-	/**
-	 * 
-	 * This function delete a photo from the database
-	 * 
-	 * @param photo the photo to delete from the database
-	 * @return true if the delete is correct
-	 * @return false if and error occurs during the delete
-	 */
-	public static boolean deleteAirplanePhoto(AirplanePhoto photo){
+
+	private static Session session;
+	
+	@SuppressWarnings("unchecked")
+	public static List<AirplanePhoto> loadAirplanePhotoList(int airplaneId){
+		List<AirplanePhoto> flightList = null;
 		try{
-			
 			ConnectHibernate.before();
-			session = ConnectHibernate.getSession();
-			session.delete(photo);
-			ConnectHibernate.after();
-			return true;
-			
+			session = ConnectHibernate.getSession();			
+			Query query = session.createQuery("select photoList from Airplane where airplaneId ="+airplaneId);
+			flightList = query.getResultList();
+//			ImageWrapper imgNew = (ImageWrapper)session.get(ImageWrapper.class, 1);
+//			byte[] bAvatar = imgNew.getData();
+//			 
+//			try{
+//			    FileOutputStream fos = new FileOutputStream("C:\temp\test.png"); 
+//			    fos.write(bAvatar);
+//			    fos.close();
+//			}catch(Exception e){
+//			    e.printStackTrace();
+//			}
 		}catch(Exception e){
-			session.getTransaction().rollback();
-			ConnectHibernate.after();
-			return false;
-		}
-	}
-	
-	
-	/**
-	 * 
-	 * This function load all the photos of the database
-	 * 
-	 * @return the list of photos if the load is correct
-	 * @return null if an error occurs during the load
-	 */
-	public static List<AirplanePhoto> loadAllAirplanePhotos() {
-		List<AirplanePhoto> photoList = null;
-		try {
-			ConnectHibernate.before();
-			session = ConnectHibernate.getSession();
-			@SuppressWarnings("unchecked")
-			TypedQuery<AirplanePhoto> query = session.createQuery("from AirplanePhoto");
-			photoList = query.getResultList();
-		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		ConnectHibernate.after();
-
-		
-		return photoList;
+		ConnectHibernate.after();		
+		return flightList;
 	}
 
 }
