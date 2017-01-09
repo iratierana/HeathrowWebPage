809076f526f05766abaf31ab72ca9796769829a3
iratierana <irati.erana@alumni.mondragon.edu>
2017-01-09 14:01:58 +0100

    Correcting errors: MyFlightsAction

--- src/actions/passengerMenu/MyFlightsAction.java
+++ src/actions/passengerMenu/MyFlightsAction.java
@@ -9,41 +9,56 @@ import domain.dao.DAOPassanger;
 import domain.model.Flight;
 import domain.model.Passanger;
 
+// TODO: Auto-generated Javadoc
 /**
- * 
+ * The Class MyFlightsAction.
+ *
  * @author Xabier Jauregi
- * @author Irati Eraña
- * @author Mikel Arizmendiarrieta 
+ * @author Irati Erana
+ * @author Mikel Arizmendiarrieta
  * @version 1.0
  * @since   2016-12-13
  * 
- * Class where are made all the processes and loads for loading the user flights.
- *  
+ * Class where are made all the processes 
+ * and loads for loading the user flights.
  */
 public class MyFlightsAction {
 	
+	/** The my flight list. */
 	private List<Flight> myFlightList = null;
+	
+	/** The session. */
 	@SuppressWarnings("rawtypes")
-	Map session = ActionContext.getContext().getSession();	
+	private Map session = ActionContext.getContext().getSession();	
 	
 	/**
-	 * 
-	 * This function loads all the fights of the user in session and save it in the myFlightList
-	 * to access from the jsp file
-	 * 
-	 * @return the page wehere we want to go
+	 * This function loads all the fights of the user 
+	 * in session and save it in the myFlightList
+	 * to access from the jsp file.
+	 *
+	 * @return the page where we want to go
 	 */
-	public String execute(){
+	public String execute() {
 		Passanger p = (Passanger) session.get("loggedPassanger");
 		myFlightList = DAOPassanger.loadPassangerFlights(p.getPassangerId());
 		return "myFlights";
 	}
 
+	/**
+	 * Gets the my flight list.
+	 *
+	 * @return the my flight list
+	 */
 	public List<Flight> getMyFlightList() {
 		return myFlightList;
 	}
 
-	public void setMyFlightList(List<Flight> myFlightList) {
+	/**
+	 * Sets the my flight list.
+	 *
+	 * @param myFlightList the new my flight list
+	 */
+	public void setMyFlightList(final List<Flight> myFlightList) {
 		this.myFlightList = myFlightList;
 	}
 	
