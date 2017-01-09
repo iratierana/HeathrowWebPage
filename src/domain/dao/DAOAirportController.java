ceb2b2e95eb50a0b7f33882f9046a5542af3e633
iratierana <irati.erana@alumni.mondragon.edu>
2017-01-09 16:00:33 +0100

    correcting errors: DAOAirportController

--- src/domain/dao/DAOAirportController.java
+++ src/domain/dao/DAOAirportController.java
@@ -10,49 +10,51 @@ import configurations.ConnectHibernate;
 import domain.model.AirportController;
 
 
+// TODO: Auto-generated Javadoc
 /**
- * 
+ * The Class DAOAirportController.
+ *
  * @author Xabier Jauregi
  * @author Irati Eraña
- * @author Mikel Arizmendiarrieta 
+ * @author Mikel Arizmendiarrieta
  * @version 1.0
  * @since   2016-12-13
  * 
- * Class where are all the needed functions related with AirportController, in order to work with the database
- *  
+ * Class where are all the needed functions related with 
+ * AirportController, in order to work with the database
  */
 public class DAOAirportController {
 	
+/** The session. */
 private static Session session;
 	
 	/**
-	 * 
-	 * This function loads a specific airport controller
-	 * 
+	 * This function loads a specific airport controller.
+	 *
 	 * @param username the username of the airport controller
 	 * @param pass the password of the airport controller
 	 * @return th object of the airport controller, else null
 	 */
 	@SuppressWarnings("unchecked")
-	public static AirportController loadAirportController(String username, String pass){
+	public static AirportController loadAirportController(final String username, final String pass) {
 		List<AirportController> controllerList = null;
 		AirportController aC = new AirportController();
 		aC = null;
-		try{
+		try {
 			ConnectHibernate.before();
 			session = ConnectHibernate.getSession();
 			
-			TypedQuery<AirportController> query = session.createQuery("from AirportController where password='"+pass+"' and username='"+username+"'");
+			TypedQuery<AirportController> query = session.createQuery("from AirportController where password='" + pass + "' and username='" + username + "'");
 			controllerList = query.getResultList();
-			if(!controllerList.isEmpty()){
-				aC=controllerList.get(0);
+			if (!controllerList.isEmpty()) {
+				aC = controllerList.get(0);
 			}
 			ConnectHibernate.after();
 			
-		}catch (Exception e) {
+		} catch (Exception e) {
 			session.getTransaction().rollback();
 			return null;
-		}finally {
+		} finally {
 			ConnectHibernate.after();
 
 		}
