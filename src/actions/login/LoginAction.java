08f60478fc21577b0449bd99140182b2cd741677
iratierana <irati.erana@alumni.mondragon.edu>
2017-01-09 12:57:10 +0100

    Correcting mistakes: LoginAction

--- src/actions/login/LoginAction.java
+++ src/actions/login/LoginAction.java
@@ -15,27 +15,37 @@ import domain.model.Airplane;
 import domain.model.AirportController;
 import domain.model.Passanger;
 
+// TODO: Auto-generated Javadoc
 /**
- * 
+ * The Class LoginAction.
+ *
  * @author Xabier Jauregi
  * @author Irati Erana
- * @author Mikel Arizmendiarrieta 
+ * @author Mikel Arizmendiarrieta
  * @version 1.0
  * @since   2016-12-13
  * 
  * Class where are made all the processes and loads of the login process.
- *  
  */
-public class LoginAction{
+public class LoginAction {
 	
+	/** The username. */
 	//Login paginatik jasotako username eta passaword 
 	private String username;
+	
+	/** The password. */
 	private String password;
 	
-	private Passanger loggedPassanger=null;
-	private AirportController loggedAirportController=null;
-	private AirlineManager loggedAirlineManager=null;
+	/** The logged passanger. */
+	private Passanger loggedPassanger = null;
 	
+	/** The logged airport controller. */
+	private AirportController loggedAirportController = null;
+	
+	/** The logged airline manager. */
+	private AirlineManager loggedAirlineManager = null;
+	
+	/** The airplane list. */
 	private List<Airplane> airplaneList = null;
 	
 	/**
@@ -71,21 +81,21 @@ public class LoginAction{
 			listener.start();
 			return "airportController";
 			
-		}else if(loggedAirlineManager != null){
+		} else if (loggedAirlineManager != null) {
 			session.put("loggedPassanger", null);
 			session.put("loggedAirportController", null);
 			session.put("loggedAirlineManager", this.loggedAirlineManager);
 						
 			airplaneList = DAOAirplane.loadAirplanesOfAirline(loggedAirlineManager.getAirlineManagerId());
 			return "airlineManager"; 
-		}else{
+		} else {
 			return "error";
 		}
 	}
 	
 	
 	/**
-	 * This function clears the user that was previously loaded in the session
+	 * This function clears the user that was previously loaded in the session.
 	 */
 	@SuppressWarnings("rawtypes")
 	private void clearAllLoggedUsers() {
@@ -102,34 +112,64 @@ public class LoginAction{
 	 * 
 	 * @return the name of the page we want to go
 	 */
-	public String register(){
+	public String register() {
 		return "register";
 	}	
 	
 	
 	
+	/**
+	 * Gets the airplane list.
+	 *
+	 * @return the airplane list
+	 */
 	public List<Airplane> getAirplaneList() {
 		return airplaneList;
 	}
 
 
-	public void setAirplaneList(List<Airplane> airplaneList) {
+	/**
+	 * Sets the airplane list.
+	 *
+	 * @param airplaneList the new airplane list
+	 */
+	public void setAirplaneList(final List<Airplane> airplaneList) {
 		this.airplaneList = airplaneList;
 	}
 
 
+	/**
+	 * Gets the username.
+	 *
+	 * @return the username
+	 */
 	public String getUsername() {
 		return username;
 	}
 
+	/**
+	 * Sets the username.
+	 *
+	 * @param username the new username
+	 */
 	public void setUsername(String username) {
 		this.username = username;
 	}
 
+	/**
+	 * Gets the password.
+	 *
+	 * @return the password
+	 */
 	public String getPassword() {
 		return password;
 	}
 
+	/**
+	 * Sets the password.
+	 *
+	 * @param password the new password
+	 */
 	public void setPassword(String password) {
 		this.password = password;
 	}
